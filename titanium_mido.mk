d7ac40ea56a99b8cbe4a8602d4d0aed858714693
Ashay Kapadiya <kapadiyaashay@gmail.com>
2020-03-23 12:43:14 +0530

    Update titanium_mido.mk

--- titanium_mido.mk
+++ titanium_mido.mk
@@ -38,5 +38,9 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="mido-user 7.0 NRD90M V9.6.1.0.NCFMIFD release-keys"
 
+# Maintainer
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.titanium.maintainer=Ashay Kapadiya
+
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "xiaomi/mido/mido:7.0/NRD90M/V9.6.1.0.NCFMIFD:user/release-keys"
